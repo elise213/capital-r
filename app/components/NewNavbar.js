f65d6f2b53fa628099f7912d7e88112b9f2711c4
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-08-20 11:54:26 -0500

    saving progress

--- app/components/NewNavbar.js
+++ app/components/NewNavbar.js
@@ -5,24 +5,22 @@ import styles from '../styles/navbar.css';
 
 const NewNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-expand-md navbar-light" id="navbar">
-      <div className="container-fluid">
-        <div>
-          <span className="nav-item">
-            <Link href="/" className="nav-btn">HOME</Link>
-          </span>
-
-          <span className="nav-item">
-            <Link href="/services" className="nav-btn">SERVICES</Link>
-          </span>
-          <span className="nav-item">
-            <Link href="/portfolio" className="nav-btn">PORTFOLIO</Link>
-          </span>
-          <span className="nav-item">
-            <Link href="/#contact-section" className="nav-btn">CONTACT</Link>
-          </span>
-        </div>
+    <nav className="navbar" id="navbar">
+      <div className="navbar-content">
+        <span className="nav-item">
+          <Link href="/" className="nav-btn">HOME</Link>
+        </span>
+        <span className="nav-item">
+          <Link href="/services" className="nav-btn">SERVICES</Link>
+        </span>
+        <span className="nav-item">
+          <Link href="/portfolio" className="nav-btn">PORTFOLIO</Link>
+        </span>
+        <span className="nav-item">
+          <Link href="/#contact-section" className="nav-btn">CONTACT</Link>
+        </span>
       </div>
+
     </nav>
   );
 };
