18b4f9db83d7f4caf92ac050c9d6a3f83d0d66cf
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-09-26 08:53:25 -0500

    progress

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import { FaPhone } from "react-icons/fa6";
 import { FaRegEnvelope } from "react-icons/fa6";
 import { FaMapPin } from "react-icons/fa6";
 
 export const Footer = () => (
   <footer className="">
-
     <div className="footer">
       <div className="new-foot">
         <div className="foot-details">
           <div className="foot">
-            < FaPhone className="footer-icon" />
-            <p className="foot-info">
-              305-663-6633
-            </p>
+            <FaPhone className="footer-icon" />
+            <p className="foot-info">305-663-6633</p>
           </div>
           <div className="foot">
             <FaRegEnvelope className="footer-icon" />
-            <p className="foot-info">
-              info@capital-realty.com
-            </p>
+            <p className="foot-info">info@capital-realty.com</p>
           </div>
           <div className="foot">
             <FaMapPin className="footer-icon" />
             <p className="foot-info">
-              4601 Ponce de Leon Blvd.<br />
+              4601 Ponce de Leon Blvd.
+              <br />
               Coral Gables, Florida 33146
             </p>
           </div>
@@ -35,7 +31,13 @@ export const Footer = () => (
         </div>
       </div>
       <div>
-        <Image width={110} height={50} className="footer-logo" src={"/img/lotus4.png"} alt=""></Image>
+        <Image
+          width={95}
+          height={30}
+          className="footer-logo"
+          src={"/img/brandlotus.png"}
+          alt=""
+        ></Image>
       </div>
     </div>
   </footer>
