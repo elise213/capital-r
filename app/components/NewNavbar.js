459171d68c3571ee39c896c182e95a651a9fe1ba
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-09-26 16:06:53 -0500

    navbar conditional rendering

--- app/components/NewNavbar.js
+++ app/components/NewNavbar.js
@@ -1,32 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import styles from '../styles/navbar.css';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "../styles/navbar.css";
 
 const NewNavbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(true);
   const [windowWidth, setWindowWidth] = useState(0);
+  const navOpen = windowWidth <= 900;
 
+  const [isNavOpen, setIsNavOpen] = useState(!navOpen);
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
   const smoothScrollToContact = () => {
-    const contactSection = document.getElementById('contact-section');
-    if (contactSection && window.location.pathname === '/') {
-      // If you are already on the home page, just scroll to the contact section
-      contactSection.scrollIntoView({ behavior: 'smooth' });
+    const contactSection = document.getElementById("contact-section");
+    if (contactSection && window.location.pathname === "/") {
+      contactSection.scrollIntoView({ behavior: "smooth" });
     } else {
-      // If you are not on the home page, navigate to the home page and scroll to the contact section
-      window.location.href = '/#contact-section';
+      window.location.href = "/#contact-section";
     }
   };
 
-  console.log("NAV OPEN", isNavOpen)
-
-
   useEffect(() => {
     setWindowWidth(window.innerWidth);
-
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       if (window.innerWidth > 900) {
@@ -35,11 +30,9 @@ const NewNavbar = () => {
       if (window.innerWidth < 900) {
         setIsNavOpen(false);
       }
-      console.log("NAV OPEN", isNavOpen)
+      console.log("NAV OPEN", isNavOpen);
     };
-
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -51,30 +44,40 @@ const NewNavbar = () => {
     }
   };
 
-
   return (
     <nav className="new-navbar">
-      <div className={`menu-icon ${isNavOpen ? 'x-styling' : ''}`} onClick={toggleNav}>
-        {isNavOpen ? 'X' : '☰'}
+      <div
+        className={`menu-icon ${isNavOpen ? "x-styling" : ""}`}
+        onClick={toggleNav}
+      >
+        {isNavOpen ? "X" : "☰"}
       </div>
 
-      <div className={`navbar-content ${isNavOpen ? 'open' : ''}`}>
+      <div className={`navbar-content ${isNavOpen ? "open" : ""}`}>
         <span className="nav-item" onClick={closeNav}>
           <Link href="/">HOME</Link>
+        </span>
+        <span className="nav-item" onClick={closeNav}>
+          <Link href="/services">SERVICES</Link>
         </span>
         <span className="nav-item" onClick={closeNav}>
           <Link href="/portfolio">PORTFOLIO</Link>
         </span>
         <span className="nav-item" onClick={closeNav}>
-          <Link href="/services">SERVICES</Link>
+          <Link href="/bio">LEADERSHIP</Link>
         </span>
-        <span className="nav-item" onClick={() => { closeNav(); smoothScrollToContact(); }}>
+        <span
+          className="nav-item"
+          onClick={() => {
+            closeNav();
+            smoothScrollToContact();
+          }}
+        >
           <a>CONTACT</a>
         </span>
       </div>
     </nav>
   );
-
 };
 
 export default NewNavbar;
